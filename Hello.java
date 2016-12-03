98379c6ae001dc181fcf27b826c378b15d430345
오하늬/j1_201611086 <gkdud644@naver.com>
2016-12-04 01:27:08 +0900

    문제00-5: 객체를 배열로 저장해서, sayHello 하기

--- Hello.java
+++ Hello.java
@@ -1,9 +1,22 @@
+package com.sd.ch8;
 class Hello{
-	String toWhom="world";
-	Hello(){}
-	void sayHello(){
-		System.out.println("hello"+toWhom);
+	private String name;
+	public Hello(String name){
+		this.name=name;
 	}
+	public void sayHello(){
+		System.out.println("hello "+name);
+	}
+	public static void main(String[] args){
+		Hello[] h=new Hello[5];
+		h[0]=new Hello("ohn");
+		h[1]=new Hello("ohn1");
+		h[2]=new Hello("ohn2");
+		h[3]=new Hello("ohn3");
+		h[4]=new Hello("ohn4");
+		for(int i=0;i<h.length;i++){
+			h[i].sayHello();
+		}
+	}
+
 }
-Hello h=new Hello();
-h.sayHello();
